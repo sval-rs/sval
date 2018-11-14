8cf43cf8c4e853b2d3b4da03d102a5c1cbf3a175
Ashley Mannix <ashleymannix@live.com.au>
2018-11-14 12:54:38 +1000

    include serde docs

--- ci/travis.sh
+++ ci/travis.sh
@@ -9,7 +9,7 @@ BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; els
 if [ "$BRANCH" == "master" ]; then
     echo "uploading crate docs"
 
-    cargo doc -p val
+    cargo doc -p val --features serde_std
 
     REV=$(git rev-parse --short HEAD)
     cd target/doc
