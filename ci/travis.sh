0870b19117b9271db008a8d788c8000a3c07f350
Ashley Mannix <ashleymannix@live.com.au>
2018-12-03 12:13:51 +1000

    do independent json builds

--- ci/travis.sh
+++ ci/travis.sh
@@ -6,4 +6,10 @@ cargo test --verbose
 cargo test --features std
 cargo test --features serde
 cargo test --all-features
+
+pushd json
+cargo build --verbose
+cargo build --features std
+popd
+
 cargo test --all --exclude sval_json_benches
