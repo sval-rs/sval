2b861280fe74f5c6d27813dd3a9c41d69c38e530
Ashley Mannix <ashleymannix@live.com.au>
2018-12-06 12:10:34 +1000

    Update docs upload location (#10)

--- ci/publish-docs.sh
+++ ci/publish-docs.sh
@@ -16,7 +16,7 @@ if [ "$BRANCH" == "master" ]; then
     REV=$(git rev-parse --short HEAD)
     cd target/doc
     git init
-    git remote add upstream "https://$GH_TOKEN@github.com/KodrAus/sval"
+    git remote add upstream "https://$GH_TOKEN@github.com/sval-rs/sval"
     git config user.name "sval"
     git config user.email "travis@sval.rs"
     git add -A .
