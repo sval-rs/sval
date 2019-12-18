84a0f7de3e082e8578b2fb4f9b9c5bd96c7a067e
Ashley Mannix <ashleymannix@live.com.au>
2019-12-19 09:54:11 +1000

    fix crate versions

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ Add `sval` to your `Cargo.toml`:
 
 ```toml,ignore
 [dependencies.sval]
-version = "0.4.7"
+version = "0.5.0"
 ```
 
 # Supported formats
@@ -165,7 +165,7 @@ fn with_value(value: impl sval::Value) {
 ```
 */
 
-#![doc(html_root_url = "https://docs.rs/sval/0.4.7")]
+#![doc(html_root_url = "https://docs.rs/sval/0.5.0")]
 #![no_std]
 
 #[doc(hidden)]
