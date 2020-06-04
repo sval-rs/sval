c432b60cdafeb0dc3bb4f90f4ecd20cfacf72391
Ashley Mannix <ashleymannix@live.com.au>
2020-06-04 22:22:43 +1000

    prepare for 0.5.2 release (#86)

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ Add `sval` to your `Cargo.toml`:
 
 ```toml,ignore
 [dependencies.sval]
-version = "0.5.1"
+version = "0.5.2"
 ```
 
 # Supported formats
@@ -157,7 +157,7 @@ fn with_value(value: impl sval::Value) {
 ```
 */
 
-#![doc(html_root_url = "https://docs.rs/sval/0.5.1")]
+#![doc(html_root_url = "https://docs.rs/sval/0.5.2")]
 #![no_std]
 
 #[doc(hidden)]
