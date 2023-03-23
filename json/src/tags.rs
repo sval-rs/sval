6e258acb01b3da7a717dbe86c31f91efff181246
KodrAus <kodraus@hey.com>
2023-03-24 07:40:49 +1000

    add some docs to tags

--- json/src/tags.rs
+++ json/src/tags.rs
@@ -4,6 +4,10 @@ Tags for JSON-specific types.
 
 /**
 A tag for strings that either don't contain characters that need escaping or are already escaped.
+
+# Valid datatypes
+
+- `text`
 */
 pub const JSON_TEXT: sval::Tag = sval::Tag::new("JSON_TEXT");
 
@@ -14,5 +18,9 @@ This tag is a sub-type of [`sval::tags::NUMBER`] that:
 
 - Does not contain leading zeroes.
 - Does not use the `+` sign.
+
+# Valid datatypes
+
+- `text`
 */
 pub const JSON_NUMBER: sval::Tag = sval::Tag::new("JSON_NUMBER");
