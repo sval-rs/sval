3be13fea712d9d2cb9274f4d191719d16d3d24a9
KodrAus <kodraus@hey.com>
2023-04-13 11:54:41 +1000

    add some more benches for borrowed data

--- buffer/benches/value.rs
+++ buffer/benches/value.rs
@@ -16,6 +16,62 @@ fn owned_data() -> OwnedData {
 }
 
 #[cfg(feature = "alloc")]
+#[derive(sval_derive::Value)]
+struct BorrowedData<'a> {
+    id: i32,
+    title: &'a str,
+    attributes: &'a [&'a str],
+}
+
+#[cfg(feature = "alloc")]
+fn borrowed_data() -> BorrowedData<'static> {
+    BorrowedData { id:  42, title: "A very important document", attributes: &["#1", "#2", "#3"] }
+}
+
+#[cfg(feature = "alloc")]
+#[bench]
+fn borrowed(b: &mut test::Bencher) {
+    b.iter(|| borrowed_data())
+}
+
+#[cfg(feature = "alloc")]
+#[bench]
+fn borrowed_collect_ref(b: &mut test::Bencher) {
+    b.iter(|| {
+        let data = borrowed_data();
+        test::black_box(sval_buffer::ValueBuf::collect(&data).unwrap());
+    })
+}
+
+#[cfg(feature = "alloc")]
+#[bench]
+fn borrowed_collect(b: &mut test::Bencher) {
+    b.iter(|| {
+        let data = borrowed_data();
+        test::black_box(sval_buffer::ValueBuf::collect_owned(data).unwrap());
+    })
+}
+
+#[cfg(feature = "alloc")]
+#[bench]
+fn borrowed_collect_ref_to_owned(b: &mut test::Bencher) {
+    b.iter(|| {
+        let data = borrowed_data();
+        sval_buffer::ValueBuf::collect(&data).unwrap().into_owned().unwrap()
+    })
+}
+
+#[cfg(feature = "alloc")]
+#[bench]
+fn borrowed_collect_to_owned(b: &mut test::Bencher) {
+    b.iter(|| {
+        let data = borrowed_data();
+        sval_buffer::ValueBuf::collect_owned(&data).unwrap().into_owned().unwrap()
+    })
+}
+
+
+#[cfg(feature = "alloc")]
 #[bench]
 fn owned(b: &mut test::Bencher) {
     b.iter(|| owned_data())
